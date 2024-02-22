c5b05415e4ca1a25dab6f79466b6c60e8fbeb812
Marc <seawerst@users.noreply.github.com>
2024-02-21 19:02:02 -0500

    Adding VS Code as supported host

--- PSMenu/Private/Test-HostSupported.ps1
+++ PSMenu/Private/Test-HostSupported.ps1
@@ -1,5 +1,5 @@
 function Test-HostSupported() {
-    $Whitelist = @("ConsoleHost")
+    $Whitelist = @("ConsoleHost","Visual Studio Code Host")
 
     if ($Whitelist -inotcontains $Host.Name) {
         Throw "This host is $($Host.Name) and does not support an interactive menu."
